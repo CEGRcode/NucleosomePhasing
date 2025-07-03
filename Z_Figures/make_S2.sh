8ae08629a812bef4dfbfcf48555d3bb126ad3e30
HeninChen <46940074+HeninChen@users.noreply.github.com>
2025-07-03 02:53:44 -0400

    Add files via upload

--- Z_Figures/make_S2.sh
+++ Z_Figures/make_S2.sh
@@ -55,68 +55,72 @@ cp $LIBRARY/WebLogos/ZNF263_MA0528.1_logo.eps	S2/a
 
 
 # Composites
-cp $LIBRARY/BI_Pileups/MA1632/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0605/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0833/Composites/*.out S2/a
-cp $LIBRARY//BI_Pileups/MA0834/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0464/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0018/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0609/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0138/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0162/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0473/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0641/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0028/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0592/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0492/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0495/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0496/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0058/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA1522/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA1640/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0620/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0147/Composites/*.out S2/a
-cp $LIBRARY//BI_Pileups/MA1527/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA1537/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0509/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0782/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0138/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0509/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0079/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0080/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0526/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0527/Composites/*.out S2/a
-cp $LIBRARY//BI_Pileups/MA1124/Composites/*.out S2/a
-cp $LIBRARY/BI_Pileups/MA0528/Composites/*.out S2/a
-
-# ===============================================================================================================================
-[ -d S2 ] || mkdir S2
-[ -d S2/b ] || mkdir S2/b
-cp $LIBRARY/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile1_1000bp/Composites/*.out S2/b
-cp $LIBRARY/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile2_1000bp/Composites/*.out S2/b
-cp $LIBRARY/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile3_1000bp/Composites/*.out S2/b
-cp $LIBRARY/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile4_1000bp/Composites/*.out S2/b
-cp $LIBRARY/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category1_1000bp/Composites/*.out S2/b
-cp $LIBRARY/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category2_1000bp/Composites/*.out S2/b
-cp $LIBRARY/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category3_1000bp/Composites/*.out S2/b
-cp $LIBRARY/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category4_1000bp/Composites/*.out S2/b
-
+cp $WRK/Library/BI_Pileups/MA1632/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0605/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0833/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0834/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0464/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0018/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0609/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0139/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0162/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0473/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0641/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0028/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0592/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0492/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0495/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0496/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0058/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA1522/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA1640/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0620/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0147/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA1527/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA1537/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0506/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0782/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0138/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0509/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0079/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0080/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0526/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0527/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA1124/Composites/*.out S2/a
+cp $WRK/Library/BI_Pileups/MA0528/Composites/*.out S2/a
 
 # ===============================================================================================================================
 [ -d S2 ] || mkdir S2
 [ -d S2/c ] || mkdir S2/c
-cp $LIBRARY/ATF2_MA1632.2_K562-specific-Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/ATF2_MA1632.2_Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/CREM_MA0609.3_K562-specific-Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/CREM_MA0609.3_Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/CTCF_MA1929.1_K562-specific-Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/CTCF_MA1929.1_Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/ZNF263_MA0528.1_K562-specific-Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/ZNF263_MA0528.1_Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/ZKSCAN1_MA1585.1_K562-specific-Unbound_1000bp/Composites/*.out S2/c
-cp  $LIBRARY/ZKSCAN1_MA1585.1_Unbound_1000bp/Composites/*.out S2/c
-	
+cp $WRK/Library/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile1_1000bp/Composites/*.out S2/c
+cp $WRK/Library/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile2_1000bp/Composites/*.out S2/c
+cp $WRK/Library/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile3_1000bp/Composites/*.out S2/c
+cp $WRK/Library/CTCF_MA1929.1_SORT-TFnucRatio_GROUP-Quartile4_1000bp/Composites/*.out S2/c
+cp $WRK/Library/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category1_1000bp/Composites/*.out S2/c
+cp $WRK/Library/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category2_1000bp/Composites/*.out S2/c
+cp $WRK/Library/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category3_1000bp/Composites/*.out S2/c
+cp $WRK/Library/MA1929_1_mm10_intersected_MPE-seq10min_164bp_category4_1000bp/Composites/*.out S2/c
 
+
+# ===============================================================================================================================
+[ -d S2 ] || mkdir S2
+[ -d S2/d ] || mkdir S2/d
+cp $WRK/Library/ATF2_MA1632.2_K562-specific-Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/ATF2_MA1632.2_Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/CREM_MA0609.3_K562-specific-Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/CREM_MA0609.3_Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/CTCF_MA1929.1_K562-specific-Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/CTCF_MA1929.1_Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/ZNF263_MA0528.1_K562-specific-Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/ZNF263_MA0528.1_Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/ZKSCAN1_MA1585.1_K562-specific-Unbound_1000bp/Composites/*.out S2/d
+cp $WRK/Library/ZKSCAN1_MA1585.1_Unbound_1000bp/Composites/*.out S2/d
+	
+# ===============================================================================================================================
+[ -d S2 ] || mkdir S2
+[ -d S2/d ] || mkdir S2/b
+cp $WRK/Library/CTCF_MA1929.1/SCORES/*.out S2/d
+	
 
 
 
